19605d1063d12b0fd7e9edd422719d8b13147c32
Constatplus <info@capital-immo.expert>
2026-07-21 14:02:57 +0200

    Make main layout responsive on mobile

--- lib/features/layout/main_layout.dart
+++ lib/features/layout/main_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import '../home/widgets/sidebar.dart';
 
 class MainLayout extends StatelessWidget {
@@ -23,24 +24,71 @@ class MainLayout extends StatelessWidget {
     this.onAdmin,
   });
 
+  HomeSidebar _buildSidebar() {
+    return HomeSidebar(
+      onEntry: onEntry,
+      onExit: onExit,
+      onBeforeWorks: onBeforeWorks,
+      onAfterWorks: onAfterWorks,
+      onFolders: onFolders,
+      onSettings: onSettings,
+      onAdmin: onAdmin,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFF4F8FA),
-      body: Row(
-        children: [
-          HomeSidebar(
-            onEntry: onEntry,
-            onExit: onExit,
-            onBeforeWorks: onBeforeWorks,
-            onAfterWorks: onAfterWorks,
-            onFolders: onFolders,
-            onSettings: onSettings,
-            onAdmin: onAdmin,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final isMobile = constraints.maxWidth < 700;
+
+        if (isMobile) {
+          return Scaffold(
+            backgroundColor: const Color(0xFFF4F8FA),
+            drawer: Drawer(
+              width: 285,
+              child: _buildSidebar(),
+            ),
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              surfaceTintColor: Colors.white,
+              elevation: 0,
+              titleSpacing: 0,
+              title: const Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Constat',
+                      style: TextStyle(
+                        color: Color(0xFF0F172A),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text: '+',
+                      style: TextStyle(
+                        color: Color(0xFF1264F6),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            body: SafeArea(top: false, child: child),
+          );
+        }
+
+        return Scaffold(
+          backgroundColor: const Color(0xFFF4F8FA),
+          body: Row(
+            children: [
+              _buildSidebar(),
+              Expanded(child: SafeArea(child: child)),
+            ],
           ),
-          Expanded(child: SafeArea(child: child)),
-        ],
-      ),
+        );
+      },
     );
   }
 }
